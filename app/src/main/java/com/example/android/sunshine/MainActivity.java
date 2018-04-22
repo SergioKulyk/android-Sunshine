2feb929a08e213f21c93b6fd671e8770b538635d
Sergio Kulyk <sergiokulyk98@gmail.com>
2018-04-22 20:09:49 +0300

    1.2 Set text for weather TextView data.

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,25 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    /** View for template text */
+    private TextView mWeatherDataTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
-
-        // TODO (3) Create an array of Strings that contain fake weather data
-
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        // Find and get a reference to the weather display TextView by id "tv_weather_data"
+        mWeatherDataTextView = findViewById(R.id.tv_weather_data);
+        // Create an array of Strings that contain fake weather data
+        String[] fakeWeather = { "Sun", "Rain", "Clouds"};
+        // Append each String from the fake weather data array to the TextView
+        for (String weatherItem : fakeWeather) {
+            mWeatherDataTextView.append(weatherItem + "\n\n\n");
+        }
     }
 }
